b1e2b5f23f8cdfd13e1618062e20c27007516a3b
jdforsythecb <jdforsythecb@gmail.com>
2013-12-23 15:34:19 -0500

    initial

---
+++ Dollar.jsx
@@ -0,0 +1,48 @@
+﻿#target indesign
+
+// load the print by preset library
+#include "/g/jdforsythe/Settings/InDesign Scripts/PrintByPreset.jsx"
+
+/*
+    you could create an instance of the PrintByPreset object, set it's property, and run it's method, like so...
+    
+    var pbp = new PrintByPreset();
+    pbp.preset = 1;
+    pbp.print();
+    
+    
+    or you can do it the lazy, memory-efficient way with chaining like so...
+*/
+
+new PrintByPreset(1).print();
+
+// wait 3 seconds
+$.sleep(3000);
+
+// send a bridgetalk message to photoshop
+
+// pops up a dialog asking the user to click ok once the printed document has loaded in photoshop
+// when they click ok, it will run the copysetting script for dollar
+
+
+
+var title = "Click ok to continue";
+        var message = "When the document appears, click OK to continue";
+        var bt = new BridgeTalk();
+            bt.target = "photoshop";
+            func = "var w = new Window('palette', '" + title + "', [0, 0, 400, 100]); \
+                            w.add('statictext', [5,5,390,50], '" + message + "'); \
+                            okButton = w.add('button', [5,40,205,90], 'OK', {name: 'ok'}); \
+                            okButton.active = true; \
+                            w.center(); \
+                            okButton.onClick = function() { \
+                                w.close(); \
+                                #include \"/g/jdforsythe/Settings/Photoshop Scripts/global/CopySetting_Dollar.jsx\"; \
+                            }; \
+                            w.center(); \
+                            w.show(); \
+                            $.sleep(100); \
+                            w.active = true; \
+                            okButton.active = true;";
+            bt.body = func;
+            bt.send();
---
+++ PrintByPreset.jsx
@@ -0,0 +1,24 @@
+﻿#target indesign
+
+var PrintByPreset = function(preset) {
+    // default to [Default] print preset
+    this.preset = preset || 0;
+    // store a reference to the active document for all methods to use
+    this.doc = app.activeDocument;
+}
+
+PrintByPreset.prototype.print = function() {
+
+    // if the document has been changed since the last save, we need a save as dialog
+    if (this.doc.saved == false) {
+        
+        // the beauty of Javascript, why use a variable?
+        this.doc.save( File.saveDialog("Specify File Location", "InDesign Files:*.indd") );
+    }
+
+    // print the file with the preset chosen
+    //app.print(File(this.doc.fullName), false, app.printerPresets[this.preset]);
+    this.doc.print (false, app.printerPresets[this.preset]);
+
+
+};
